3be5300dbcde85698b8143e55cee31f1ae35be78
juugaya <51192393+juugaya@users.noreply.github.com>
2026-05-10 15:05:54 +0900

    Add files via upload

--- mode-switch.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const params = new URLSearchParams(window.location.search);
-  const eventId = params.get("event");
-
-  if (eventId) {
-    document.getElementById("event-list").style.display = "none";
-    document.getElementById("container").style.display = "flex";
-  }
-});
